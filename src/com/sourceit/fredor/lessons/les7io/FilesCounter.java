9f8938189c4bcbe244e56f31f3d881cc48489457
Fedor Khodko <gvidoarcher@gmail.com>
2020-02-21 17:53:55 +0200

    updated

--- src/com/sourceit/fredor/lessons/les7io/FilesCounter.java
+++ src/com/sourceit/fredor/lessons/les7io/FilesCounter.java
@@ -1,27 +1,79 @@
 package com.sourceit.fredor.lessons.les7io;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FilesCounter {
 
 	private int counter;
-	private int nestingLevel;
+	private int depth;
+	private StringBuilder namesList = new StringBuilder();
+
+	private List<Integer> hashCodes = new ArrayList<>();
 
 	public int countExtensionsInDir(File dir, String extension) {
 		
-//	if (!dir.isDirectory() || dir == null || extension == null) return -1;
+		if (!dir.isDirectory() || dir == null || extension == null) return -1;
+		depth++;
 		if (dir.listFiles().length == 0) return 0;
-		nestingLevel++;
+				
 		File[] files = dir.listFiles();
 		Arrays.stream(files).forEach(obj -> {
-			if(obj.isDirectory()) {				
+			if (obj.isDirectory()) {
+			
 				countExtensionsInDir(obj, extension);
 			}
-			if (obj.getName().endsWith(extension)) counter++;
-			
+			if (obj.getName().endsWith(extension))
+				counter++;
+
 		});
+		
 		return counter;
 	}
 
+	public int countExtensionsInDir(File dir, String extension, int depth) {
+		if (!dir.isDirectory() || dir == null || extension == null) return -1;
+		depth++;
+		if (dir.listFiles().length == 0 || depth < 1) return 0;
+		if (this.depth == depth) return counter;
+		
+		File[] files = dir.listFiles();
+		Arrays.stream(files).forEach(obj -> {
+			if (obj.isDirectory()) {			    
+				countExtensionsInDir(obj, extension);
+			}
+			if (obj.getName().endsWith(extension)) {
+				counter++;
+				System.out.println(obj.getName());
+				namesList.append(obj.getName().split(".")[0] + "\n");
+			}
+
+		});
+		
+		if (this.depth == depth) return counter;
+
+		return counter;
+
+	}
+	
+	public StringBuilder getNamesList() {
+		return namesList;
+	}
+
+	
+	
+//	public void inputValidation(File dir, String extension) {
+//		if (!dir.isDirectory() || dir == null || extension == null) {
+//			System.out.println("Input is not valid.");
+//			return;
+//		} 
+//		
+//		countExtensionsInDir(dir, extension);
+//		
+//	}
+	
+	
+	
 }
--- src/com/sourceit/fredor/lessons/les7io/FilesCounterDemo.java
+++ src/com/sourceit/fredor/lessons/les7io/FilesCounterDemo.java
@@ -5,9 +5,12 @@ import java.io.File;
 public class FilesCounterDemo {
 
 	public static void main(String[] args) {
+		FilesCounter fc = new FilesCounter();
+		int qtyOfFiles = fc.countExtensionsInDir(new File(new DirectoryPathReader().getPath()), ".java");
 		
-		System.out.println(new FilesCounter().countExtensionsInDir(new File(new DirectoryPathReader().getPath()), ".txt"));
-		new FilesCounter().countExtensionsInDir(new File(new DirectoryPathReader().getPath()), ".bat");
+//		System.out.println(new FilesCounter().countExtensionsInDir(new File(new DirectoryPathReader().getPath()), ".txt"));
+		System.out.println(qtyOfFiles);
+
 
 	}
 
