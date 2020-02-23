6407cdf3067d90312fab0abef836bbbb12092e98
Fedor Khodko <gvidoarcher@gmail.com>
2020-02-23 17:45:17 +0200

    added default txt file

--- src/com/sourceit/fredor/lessons/les8io/TextEditor.java
+++ src/com/sourceit/fredor/lessons/les8io/TextEditor.java
@@ -1,11 +1,33 @@
 package com.sourceit.fredor.lessons.les8io;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 //Class functionality: 
 
 public class TextEditor {
 	
-	private Path path;
+	private Path path = Paths.get("res\\hw8\\default_file.txt");
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public String getFileContent() {
+		return null;
+	}
+	
+	public int getQtyOfSymbols() {
+		return 0;
+	}
+	
+	public int getMaxVowelLineNumber () {
+		return 0;
+	}
+	
+	public int getWordsQty() {
+		return 0;
+	}
 	
 }
