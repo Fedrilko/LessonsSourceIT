cd9c200838f47850bb42f15f72da10788a908952
Fedor Khodko <gvidoarcher@gmail.com>
2020-03-09 20:53:41 +0200

    some text

--- src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
+++ src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
@@ -1,19 +1,23 @@
 package com.sourceit.fredor.lessons.les10CollsAndConcur;
 
-public class LRUCache {
+import java.util.HashMap;
+
+public class LRUCache extends HashMap{
 
 	private int capacity;
 	
 	public LRUCache(int capacity) {
+		super(capacity, 1.0f);
 		this.capacity = capacity;
 	}
 
 	public static void main(String[] args) {
-
+		
 	}
+	
 
 	public int get(int key) {
-		return 0;
+		return -1;
 	}
 	
 	public void out(int key, int value) {
