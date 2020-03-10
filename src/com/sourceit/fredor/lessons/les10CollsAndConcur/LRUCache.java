3fe4e6046bae5c3b671be72229ea0c2995723a25
Fedor Khodko <gvidoarcher@gmail.com>
2020-03-10 21:40:09 +0200

    done

--- src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
+++ src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
@@ -11,37 +11,54 @@ public class LRUCache{
 	private List<Integer> buffer = new ArrayList<>();
 	
 	public LRUCache(int capacity) {
-//		super(capacity, 1.0f);
 		this.capacity = capacity;
 	}
 
 	public static void main(String[] args) {
-		
+		LRUCache cache = new LRUCache(2);
+		cache.put(1, 1);
+		cache.put(2, 2);
+		System.out.println(cache.get(1));
+		cache.put(3, 3);
+		System.out.println(cache.get(2));
+		cache.put(4, 4);
+		System.out.println(cache.get(1));
+		System.out.println(cache.get(3));
+		System.out.println(cache.get(4));		
 	}
 	
 
 	public int get(int key) {
-		int val = getIndex(key);
-		if (val == -1)
-			return -1;
-		else {
-			buffer.add(0, val);
-			buffer.remove(val + 1);
+		int index = getIndex(key);
+		if (index == -1)
+			return index;
+		else { //If key already presented in buffer shift element to the beginning
+			buffer.add(0, index);
+			buffer.remove(index + 1);
 		}
 		return table.get(key);
 	}
 
 	public void put(int key, int value) {
 		Integer returnedValue = table.put(key, value);
-		addToBuffer(key);
-		
+		int index = getIndex(key); //Intention of declaring this variable is to check the presence of key in the buffer
+		if (returnedValue == null || index == -1)
+			addToBuffer(key);
+		else { //In case, when such key already exists in buffer put it in the beginning
+			buffer.remove(index);
+			buffer.add(0, key);
+		}
 	}
 	
-	private void addToBuffer(int key) {
-		
+	//Method adds key to buffer
+	private void addToBuffer(int key) {		
 		buffer.add(0, key);
+		if (buffer.size() > capacity) 
+			buffer.remove(capacity);
+		
 	}
 	
+	//Returns index of requested key in the buffer if it's presented there
 	private Integer getIndex(int key) {
 		for (int i = 0; i < buffer.size(); i++) {
 			if (buffer.get(i).equals(key)) return i;
