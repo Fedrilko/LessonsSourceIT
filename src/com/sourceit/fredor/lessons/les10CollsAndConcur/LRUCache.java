758704c948743904a5e6158a49721643cbb86253
Fedor Khodko <gvidoarcher@gmail.com>
2020-03-10 17:05:55 +0200

    almost done. probably

--- src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
+++ src/com/sourceit/fredor/lessons/les10CollsAndConcur/LRUCache.java
@@ -1,13 +1,17 @@
 package com.sourceit.fredor.lessons.les10CollsAndConcur;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class LRUCache extends HashMap{
+public class LRUCache{
 
 	private int capacity;
+	private HashMap<Integer, Integer> table = new HashMap<>();
+	private List<Integer> buffer = new ArrayList<>();
 	
 	public LRUCache(int capacity) {
-		super(capacity, 1.0f);
+//		super(capacity, 1.0f);
 		this.capacity = capacity;
 	}
 
@@ -17,10 +21,31 @@ public class LRUCache extends HashMap{
 	
 
 	public int get(int key) {
-		return -1;
+		int val = getIndex(key);
+		if (val == -1)
+			return -1;
+		else {
+			buffer.add(0, val);
+			buffer.remove(val + 1);
+		}
+		return table.get(key);
+	}
+
+	public void put(int key, int value) {
+		Integer returnedValue = table.put(key, value);
+		addToBuffer(key);
+		
 	}
 	
-	public void out(int key, int value) {
+	private void addToBuffer(int key) {
 		
+		buffer.add(0, key);
+	}
+	
+	private Integer getIndex(int key) {
+		for (int i = 0; i < buffer.size(); i++) {
+			if (buffer.get(i).equals(key)) return i;
+		}
+		return -1;
 	}
 }
